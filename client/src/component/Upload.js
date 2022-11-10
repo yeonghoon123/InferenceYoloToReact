c53ac245130f084e5206e8721761ea5bce7bac17
yeonghoon123 <haa3163314@naver.com>
2022-11-10 11:24:36 +0900

    client에서 보낸 파일을 server에서 읽는 기능 완성

--- client/src/component/Upload.js
+++ client/src/component/Upload.js
@@ -1,22 +1,43 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const UploadComp = () => {
     const [uploadData, setUploadData] = useState("");
+
+    /** 이미지 파일 업로드 */
     const fileImgUpload = (event) => {
-        const getImgs = event.target.files;
-        const uploadData = new FormData();
-        for(let cnt = 0; cnt < getImgs.length; cnt++){
-        }
+        const getImgs = event.target.files; // 사용자가 업로드한 사진
+        const uploadImgs = new FormData(); // 서버에 보낼 폼 생성
+
+        // 파일을 생성한 폼에 추가
+        for(let cnt = 0; cnt < getImgs.length ; cnt++){
+            uploadImgs.append("imgFile", getImgs[cnt]);
+        }   
+
+        setUploadData(uploadImgs); // 업로드할 데이터 변경
     }
 
+    /** 비디오 파일 업로드 */
     const fileVideoUpload = (event) => {
+        const getVideo = event.target.files[0]; // 사용자가 업로드한 영상
+        const uploadVideo = new FormData(); // 서버에 보낼 폼 생성
+        uploadVideo.append("imgFile", getVideo);
 
+        setUploadData(uploadVideo); // 업로드할 데이터 변경
+    }
+
+    /** 서버로 파일 전송 */
+    const uploadServer = async () => {
+        const response = await axios.post("http://localhost:8000/",uploadData,{headers: {
+            'Content-Type': 'multipart/form-data'
+          }})
+        console.log(response)
     }
     return(
         <>
-            <input type="file" accept="image/*" multiple={true} onChange={fileImgUpload}/>
-            <input type="file" onChange={fileVideoUpload}/>
-            <h1>hello world</h1>
+            <input type="file" accept="image/jpeg" multiple={true} onChange={fileImgUpload}/>
+            <input type="file" accept="video/mp4, video/mkv" onChange={fileVideoUpload}/>
+            <button onClick={uploadServer}>hello world</button>
         </>
     )
 }
--- server/app.js
+++ server/app.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
 const app = express();
 const port = 8000;
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+const upload = multer();
+
 app.get('/',(req,res) => {
     res.send("hi");
+})
+
+app.post('/', upload.any(), (req,res) => {
+    console.log(req.files);
+    res.send("hello");
 })
 
 app.listen(port, () => console.log(`${port} connect complete`));
