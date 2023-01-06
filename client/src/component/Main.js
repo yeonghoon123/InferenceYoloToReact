bf32ab8f8b55e8be5b6e3cf917dee53f389834bc
yeonghoon123 <haa3163314@naver.com>
2023-01-06 15:30:51 +0900

    inference결과 나옴

--- client/src/component/Main.js
+++ client/src/component/Main.js
@@ -4,13 +4,15 @@ import ImageArea from "./ImageArea";
 import "../css/Main.css";
 
 const Main = () => {
+    const [inferenceBase64Data, setInferenceBase64Data] = useState([]); // 업로드된 이미지의 base64
     const [srcBase64Data, setSrcBase64Data] = useState([]); // 업로드된 이미지의 base64
+    const [loading, setLoading] = useState(false);
     return(
     <>
         <div className="Main_container">
-            <UploadComp props={ {setSrcBase64Data} }/>
+            <UploadComp props={ {setLoading ,setSrcBase64Data, setInferenceBase64Data} }/>
             <div className="Main_imgarea">
-                <ImageArea props={{srcBase64Data}}/>
+                <ImageArea props={{srcBase64Data,loading, inferenceBase64Data}}/>
             </div>
         </div>
     </>
